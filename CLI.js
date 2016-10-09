9b49d18903be525d6a155c5faa97eaebc3c8d6b5
Thomas Morgana <tmorgana64@gmail.com>
2016-10-10 00:37:27 +0200

    Code for simple query method

--- CLI.js
+++ CLI.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 const program = require("commander");
 const inquirer = require("inquirer");
-
+const weather = require('node-openweather')({
+  key: "bdaf14209c7443ae1c46f1bae34a6332",
+  accuracy: "like",
+  unit: "metric",
+  language: "en"
+});
 
 program
   .version("1.0.0")
   .option("-q, --query", "Ask the weather for a location")
   .option("-f, --favourite", "Show favourite locations or save a new one")
-  .option("-c, --compare, --compare [cityA] [cityB]", "Compare the weather between 2 cities");
+  .option("-c, --compare", "Compare the weather between 2 cities");
 
 program.parse(process.argv);
 
@@ -15,12 +20,12 @@ if (program.query) {
 	inquirer.prompt([
 		{
 			type: "input",
-			message: "Entrez le nom de la ville pour laquelle vous voulez conaître la météo ",
+			message: "Entrez le nom de la ville pour laquelle vous voulez connaître la météo \n",
 			name: "location"
 		},
 		{
 			type: "list",
-			message: "Quelle periode de temps voulez vous afficher ",
+			message: "Quelle periode de temps voulez vous afficher \n",
 			name: "date",
 			choices: [
 				"aujourd'hui",
@@ -34,18 +39,87 @@ if (program.query) {
 } else if (program.favourite) {
 	favourite();
 } else if (program.compare) {
-	compare();
+  inquirer.prompt([
+		{
+      type: "list",
+			message: "Voulez vous utiliser vos favoris ou séléctionner deux nouvelles villes? \n",
+			name: "date",
+			choices: [
+				"Favoris",
+        "Libre"
+			]
+		}
+  ]).then((answer) => {
+  		if (answer == "Favoris")
+      {
+        inquirer.prompt([
+      		{
+            type: "checkbox",
+      			message: "Voulez vous utiliser vos favoris ou séléctionner deux nouvelles villes? \n",
+      			name: "date",
+      			choices: getFavourites()
+          }
+        ]).then((answers) => {
+            compare(answers);
+        })
+      } else {
+        inquirer.prompt([
+      		{
+      			type: "input",
+      			message: "Entrez le nom de la première ville à comparer\n",
+      			name: "location"
+      		},
+          {
+      			type: "input",
+      			message: "Entrez le nom de la seconde ville à comparer\n",
+      			name: "location"
+      		}
+        ]).then((answers) => {
+            compare(answers);
+        })
+      }
 }
 
 function query(location, date){
+  switch (date) {
+    case "aujourd'hui":
 
+      weather.city(location).now().then((result) => {
+        console.log("Aujourd'hui il fait " + Math.round(result.main.temp_min - 273.15) + "°C à " + location);
+      })
+      break;
+
+    case "demain":
+      weather.city(location).forecast(5).then((result) => {
+        var avgTemp = 0;
+        for(var i = 8; i < 16; i++){
+          avgTemp += result.list[i].main.temp_min;
+        }
+        avgTemp = Math.round((avgTemp/8) - 273.15);
+        console.log("Demain il fera en moyenne " + avgTemp + "°C à " + location);
+      })
+      break;
+
+    case "cette semaine":
+      weather.city(location).forecast(16).then((result) => {
+        var avgTemp = 0;
+        for(var i = 0; i < 7; i++){
+          avgTemp += result.list[i].temp.min + result.list[i].temp.max;
+        }
+        avgTemp = Math.round((avgTemp/14) - 273.15);
+        console.log("Cette semaine il fera en moyenne " + avgTemp + "°C à " + location);
+      })
+      break;
+    default:
+      console.error("You ! What have you done? You shouldn't be there !")
+  }
 }
 
 function favourite(){
 	inquirer.prompt([
 		{
 			type: "list",
-			message: "Voulez vous afficher vos favoris ou en ajouter un ?",
+			message: "Voulez vous afficher vos favoris ou en ajouter un ?\n",
 			name: "choice",
 			choices: [
 				"afficher",
@@ -59,7 +133,7 @@ function favourite(){
 			inquirer.prompt([
 				{
 					type: "input",
-					message: "Entrez le nom de la ville pour laquelle vous voulez conaître la météo ",
+					message: "Entrez le nom de la ville pour laquelle vous voulez connaître la météo\n ",
 					name: "location"
 				}
 			]).then((answer) =>{
@@ -73,5 +147,5 @@ function compare(){
 
 }
 
-function getFavourites(){}
+function getFavourites(){ return [];}
 function addFavourite(){}
