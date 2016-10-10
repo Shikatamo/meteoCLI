56cfd9f96a4e93f270c0c593fe950797247105a6
Thomas Morgana <tmorgana64@gmail.com>
2016-10-10 12:06:30 +0200

    Allow user to save and del favourites

--- CLI.js
+++ CLI.js
@@ -6,15 +6,9 @@ const weather = require('node-openweather')({
   key: "bdaf14209c7443ae1c46f1bae34a6332",
   accuracy: "like",
   units: "metric",
-  language: "en"
+  language: "en",
+  country: "France"
 });
-
-//DB initialisation
-db.open("meteo.db").then(() => {
-  return db.run("CREATE TABLE IF NOT EXISTS favourite (id, name, country)");
-}).then(() => {
-  console.log("Database created")
-})
 
 //CLI program parameters
 program
@@ -25,75 +19,80 @@ program
 
 program.parse(process.argv);
 
-if (program.query) {
-	inquirer.prompt([
-		{
-			type: "input",
-			message: "Entrez le nom de la ville pour laquelle vous voulez connaître la météo \n",
-			name: "location"
-		},
-		{
-			type: "list",
-			message: "Quelle periode de temps voulez vous afficher \n",
-			name: "date",
-			choices: [
-				"aujourd'hui",
-				"demain",
-				"cette semaine"
-			]
-		}
-	]).then((answers) => {
-		query(answers.location, answers.date);
-	});
-} else if (program.favourite) {
-	favourite();
-} else if (program.compare) {
-  inquirer.prompt([
-		{
-      type: "list",
-			message: "Voulez vous utiliser vos favoris ou séléctionner deux nouvelles villes? \n",
-			name: "date",
-			choices: [
-				"Favoris",
-        "Libre"
-			]
-		}
-  ]).then((answer) => {
-  		if (answer == "Favoris")
-      {
-        inquirer.prompt([
-      		{
-            type: "checkbox",
-      			message: "Voulez vous utiliser vos favoris ou séléctionner deux nouvelles villes? \n",
-      			name: "date",
-      			choices: getFavourites()
-          }
-        ]).then((answers) => {
-            compare(answers);
-        })
-      } else {
-        inquirer.prompt([
-      		{
-      			type: "input",
-      			message: "Entrez le nom de la première ville à comparer\n",
-      			name: "location"
-      		},
-          {
-      			type: "input",
-      			message: "Entrez le nom de la seconde ville à comparer\n",
-      			name: "location"
-      		}
-        ]).then((answers) => {
-            compare(answers);
-        })
-      }
-    })
-}
+//DB initialisation
+db.open("meteo.db").then(() => {
+  return db.run("CREATE TABLE IF NOT EXISTS favourite (id PRIMARY KEY, name UNIQUE)");
+}).then(() => {
+  if (program.query) {
+  	inquirer.prompt([
+  		{
+  			type: "input",
+  			message: "Entrez le nom de la ville pour laquelle vous voulez connaître la météo \n",
+  			name: "location"
+  		},
+  		{
+  			type: "list",
+  			message: "Quelle periode de temps voulez vous afficher \n",
+  			name: "date",
+  			choices: [
+  				"aujourd'hui",
+  				"demain",
+  				"cette semaine"
+  			]
+  		}
+  	]).then((answers) => {
+  		query(answers.location, answers.date);
+  	});
+  } else if (program.favourite) {
+  	favourite();
+  } else if (program.compare) {
+    inquirer.prompt([
+  		{
+        type: "list",
+  			message: "Voulez vous utiliser vos favoris ou séléctionner deux nouvelles villes? \n",
+  			name: "date",
+  			choices: [
+  				"Favoris",
+          "Libre"
+  			]
+  		}
+    ]).then((answer) => {
+    		if (answer == "Favoris")
+        {
+          inquirer.prompt([
+        		{
+              type: "checkbox",
+        			message: "Voulez vous utiliser vos favoris ou séléctionner deux nouvelles villes? \n",
+        			name: "date",
+        			choices: getFavourites()
+            }
+          ]).then((answers) => {
+              compare(answers);
+          })
+        } else {
+          inquirer.prompt([
+        		{
+        			type: "input",
+        			message: "Entrez le nom de la première ville à comparer\n",
+        			name: "location"
+        		},
+            {
+        			type: "input",
+        			message: "Entrez le nom de la seconde ville à comparer\n",
+        			name: "location"
+        		}
+          ]).then((answers) => {
+              compare(answers);
+          })
+        }
+      })
+  }
+})
 
 function query(location, date){
   switch (date) {
     case "aujourd'hui":
-      console.log("API call : ")
+      console.log("\nAPI call : ")
       weather.city(location).now().then((result) => {
         console.log("\nAujourd'hui il fait " + Math.round(result.main.temp_min) + "°C à " + location);
       }).catch((err) =>{
@@ -102,7 +101,7 @@ function query(location, date){
       break;
 
     case "demain":
-      console.log("API call : ")
+      console.log("\nAPI call : ")
       weather.city(location).forecast(5).then((result) => {
         var avgTemp = 0;
         for(var i = 8; i < 16; i++){
@@ -116,7 +115,7 @@ function query(location, date){
       break;
 
     case "cette semaine":
-      console.log("API call : ")
+      console.log("\nAPI call : ")
       weather.city(location).forecast(16).then((result) => {
         var avgTemp = 0;
         for(var i = 0; i < 7; i++){
@@ -137,35 +136,42 @@ function favourite(){
 	inquirer.prompt([
 		{
 			type: "list",
-			message: "Voulez vous afficher vos favoris ou en ajouter un ?\n",
+			message: "Voulez vous ajouter ou supprimer un favoris ?\n",
 			name: "choice",
 			choices: [
-				"afficher",
-				"ajouter"
+				"ajouter",
+        "supprimer"
 			]
 		}
 	]).then((answer) => {
-		if(answer.choices == "afficher"){
-			getFavourites();
-		} else {
-			inquirer.prompt([
+		if(answer.choice == "ajouter"){
+      inquirer.prompt([
 				{
 					type: "input",
 					message: "Entrez le nom de la ville pour laquelle vous voulez connaître la météo\n ",
 					name: "location"
 				}
 			]).then((answer) =>{
-				addFavourite(answer.location);
+				db.run("INSERT INTO favourite VALUES(NULL,?)", answer.location)
 			});
+		} else {
+      db.all("SELECT name FROM favourite").then((answers) => {
+        console.log(answers);
+        inquirer.prompt([
+  				{
+  					type: "list",
+  					message: "Séléctionnez le favoris à supprimer\n ",
+  					name: "favoris",
+            choices: answers
+  				}
+  			]).then((answer) =>{
+  				db.run("DELETE FROM favourite WHERE name = ?", answer.favoris)
+  			});
+      })
 		}
 	});
 }
 
 function compare(){
-
-}
-
-function getFavourites(){ return [];}
-function addFavourite(location){
 
 }
